Fixed some minor bugs and added a softmax layer.

ann.py
@@ -87,7 +87,7 @@ class Network(object):
     def __repr__(self):
         return '<Network: (layers=%s, cost_function=%s)>' % (str(self._layers), str(self._cost_function))
 
-# Inputs and outputs are column vectors
+# Inputs and outputs are row vectors
 class LinearLayer(Layer):
     def __init__(self, input_size, output_size):
         # +1 for the bias.
@@ -132,6 +132,23 @@ class SigmoidLayer(Layer):
     def __repr__(self):
         return 'Sigmoid'
 sigmoidLayer = SigmoidLayer()
+
+class SoftmaxLayer(Layer):
+  def __init__(self):
+    self._params = np.array([])
+
+  def dydx(self, x, y):
+    ret = -y.T.dot(y)
+    ret.flat[::ret.shape[0]+1] = y * (1 - y)
+    return ret
+
+  def dcostdx(self, x, y, dcostdy):
+    return dcostdy.dot(self.dydx(x, y))
+
+  def compute(self, x):
+    e = np.exp(x)
+    return e / np.sum(e)
+softmaxLayer = SoftmaxLayer()
 
 class LogisticCost(object):
     def dcostdy(self, y, y_):

ann_unittest.py
@@ -4,6 +4,18 @@ import unittest
 
 SMALL=0.000001
 
+class NumpyTestCase(unittest.TestCase):
+  def failUnlessAlmostEqual(self, first, second, places=7, msg=None):
+    if isinstance(first, np.ndarray):
+      zeros = np.zeros(first.shape)
+      if ((first-second).round(decimals=places) != zeros).any():
+        raise self.failureException(
+            msg or '%r != %r within %r places' % (first, second, places))
+    else:
+      super(NumpyTestCase, self).assertAlmostEqual(first, second, places, msg)
+
+  assertAlmostEqual = assertAlmostEquals = failUnlessAlmostEqual
+
 class TestLogisticCost(unittest.TestCase):
   def test_single_positive(self):
     predicted = np.array([[0.3]])
@@ -11,13 +23,13 @@ class TestLogisticCost(unittest.TestCase):
     gold = -np.log(0.3)
 
     cost = ann.logisticCost.compute(actual, predicted)
-    self.assertAlmostEquals(gold, cost)
+    self.assertAlmostEqual(gold, cost)
 
     cost1 = ann.logisticCost.compute(actual, predicted + SMALL)
     dcost_numeric = (cost1 - cost) / SMALL
 
     dcost = ann.logisticCost.dcostdy(actual, predicted)
-    self.assertAlmostEquals(dcost_numeric, dcost, places=4)
+    self.assertAlmostEqual(dcost_numeric[0], dcost[0][0], places=4)
 
   def test_single_negative(self):
     predicted = np.array([[0.3]])
@@ -25,13 +37,13 @@ class TestLogisticCost(unittest.TestCase):
     gold = -np.log(1 - 0.3)
 
     cost = ann.logisticCost.compute(actual, predicted)
-    self.assertAlmostEquals(gold, cost)
+    self.assertAlmostEqual(gold, cost)
 
     cost1 = ann.logisticCost.compute(actual, predicted + SMALL)
     dcost_numeric = (cost1 - cost) / SMALL
 
     dcost = ann.logisticCost.dcostdy(actual, predicted)
-    self.assertAlmostEquals(dcost_numeric, dcost, places=4)
+    self.assertAlmostEqual(dcost_numeric[0], dcost[0][0], places=4)
 
   def test_multi(self):
     predicted = np.array([0.1, 0.3, 0.7, 0.8])
@@ -48,10 +60,10 @@ class TestLogisticCost(unittest.TestCase):
     actual.shape = (4, 1)
 
     cost = ann.logisticCost.compute(actual, predicted)
-    self.assertAlmostEquals(gold[0], cost[0])
-    self.assertAlmostEquals(gold[1], cost[1])
-    self.assertAlmostEquals(gold[2], cost[2])
-    self.assertAlmostEquals(gold[3], cost[3])
+    self.assertAlmostEqual(gold[0], cost[0])
+    self.assertAlmostEqual(gold[1], cost[1])
+    self.assertAlmostEqual(gold[2], cost[2])
+    self.assertAlmostEqual(gold[3], cost[3])
 
     cost = sum(cost) / 4
 
@@ -62,29 +74,45 @@ class TestLogisticCost(unittest.TestCase):
     cost1 = sum(ann.logisticCost.compute(actual, predicted)) / 4
     predicted[idx][0] -= SMALL
     dcost_numeric = (cost1 - cost) / SMALL
-    self.assertAlmostEquals(dcost_numeric, dcost[idx][0], places=4)
+    self.assertAlmostEqual(dcost_numeric, dcost[idx][0], places=4)
 
     idx = 1
     predicted[idx][0] += SMALL
     cost1 = sum(ann.logisticCost.compute(actual, predicted)) / 4
     predicted[idx][0] -= SMALL
     dcost_numeric = (cost1 - cost) / SMALL
-    self.assertAlmostEquals(dcost_numeric, dcost[idx][0], places=4)
+    self.assertAlmostEqual(dcost_numeric, dcost[idx][0], places=4)
 
     idx = 2
     predicted[idx][0] += SMALL
     cost1 = sum(ann.logisticCost.compute(actual, predicted)) / 4
     predicted[idx][0] -= SMALL
     dcost_numeric = (cost1 - cost) / SMALL
-    self.assertAlmostEquals(dcost_numeric, dcost[idx][0], places=4)
+    self.assertAlmostEqual(dcost_numeric, dcost[idx][0], places=4)
 
     idx = 3
     predicted[idx][0] += SMALL
     cost1 = sum(ann.logisticCost.compute(actual, predicted)) / 4
     predicted[idx][0] -= SMALL
     dcost_numeric = (cost1 - cost) / SMALL
-    self.assertAlmostEquals(dcost_numeric, dcost[idx][0], places=4)
+    self.assertAlmostEqual(dcost_numeric, dcost[idx][0], places=4)
 
+class TestSoftmax(NumpyTestCase):
+  def test_dydx(self):
+    x = np.array([[1.0, -0.2, 3.5]])
+    y = ann.softmaxLayer.compute(x)
+    self.assertAlmostEqual(1.0, np.sum(y))
+
+    dydx = ann.softmaxLayer.dydx(x, y)
+    dydx_numeric = np.zeros((3, 3))
+    
+    for idx in range(3):
+      x[0][idx] += SMALL
+      y1 = ann.softmaxLayer.compute(x)
+      dydx_numeric[:,idx] = ((y1 - y) / SMALL).flatten()
+      x[0][idx] -= SMALL
+
+    self.assertAlmostEqual(dydx_numeric, dydx)
 
 class TestTraining(unittest.TestCase):
   def data1(self):